749c7e04b597ecf160751d2c2354fc64ef303768
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-10-18 13:02:04 +0200

    add home test

---
+++ src/__tests__/Home.test.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Home from "../components/Home";
+import "@testing-library/jest-dom";
+
+test("affiche une liste d’inspections", () => {
+  const mockData = {
+    inspections: [
+      { name: "Inspection 1" },
+      { name: "Inspection 2" },
+      { name: "Inspection 3" },
+    ],
+  };
+
+  render(
+    <MemoryRouter>
+      <Home inspectionData={mockData} />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText("Inspection 1")).toBeInTheDocument();
+  expect(screen.getByText("Inspection 2")).toBeInTheDocument();
+  expect(screen.getByText("Inspection 3")).toBeInTheDocument();
+});
